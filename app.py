bac3e1570a0e0b372ecdb4b233dfe1e2a4c66209
Tomoya Sakoda <tomoya.sakoda@gmail.com>
2026-04-01 23:05:12 +0900

    更新

--- app.py
+++ app.py
@@ -53,7 +53,6 @@ selected_chars = groups[st.session_state.selected_group]
 
 filtered = df[df["馬名"].astype(str).str.startswith(selected_chars)]
 
-st.subheader(f"{st.session_state.selected_group} 行")
 horse_list = filtered["馬名"].tolist()
 horse = st.radio(
     "🐎 馬を選択",
